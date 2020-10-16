6a900815a4fd537a2aa82e4c5778b398956e343a
Kaylyn Bogle <kaylyn@kaylyn.ink>
2020-10-16 16:42:10 -0700

    Make process_record_keymap work correctly

---
+++ users/kaylynb/kaylynb_split.c
@@ -0,0 +1,43 @@
+#include "kaylynb_split.h"
+
+__attribute__((weak)) bool process_record_keymap(uint16_t keycode, keyrecord_t* record) { return true; }
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    if (!process_record_keymap(keycode, record)) {
+        return false;
+    }
+
+    switch (keycode) {
+        case LOWER:
+            if (record->event.pressed) {
+                layer_on(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_LOWER);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+        case RAISE:
+            if (record->event.pressed) {
+                layer_on(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_RAISE);
+                update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+        case ADJUST:
+            if (record->event.pressed) {
+                layer_on(_ADJUST);
+            } else {
+                layer_off(_ADJUST);
+            }
+            return false;
+        case TNKRO:
+            if (record->event.pressed) {
+                keymap_config.nkro = !keymap_config.nkro;
+            }
+            return false;
+    }
+    return true;
+}
