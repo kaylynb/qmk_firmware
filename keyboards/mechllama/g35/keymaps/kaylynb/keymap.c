f5a6d195b041d884ed213555cb8b0e49fe03a977
Kaylyn Bogle <kaylyn@kaylyn.ink>
2020-10-29 23:39:21 -0700

    Change some coh2 keybinds

--- keyboards/mechllama/g35/keymaps/kaylynb/keymap.c
+++ keyboards/mechllama/g35/keymaps/kaylynb/keymap.c
@@ -89,11 +89,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_COH2] = LAYOUT(
     //┌────────┬────────┬────────┬────────┬────────┬────────┬────────┐
-        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_BSPC,
+        KC_ESC,  KC_TAB,   KC_1,    KC_2,    KC_3,    KC_4,  KC_BSPC,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
         KC_F4,   KC_F5,    KC_Q,    KC_W,    KC_E,    KC_R,  XXXXXXX,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_F1,   KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,  LCTL(KC_A),
+        KC_F1,  KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,  LCTL(KC_A),
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼
         KC_F2,   KC_F3,    KC_Z,    KC_X,    KC_C,    KC_V,  XXXXXXX,
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼
